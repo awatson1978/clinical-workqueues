170c15498a1a1478087a14c41bfcffbb8ffa6bbe
Abigail Watson <awatson1978@gmail.com>
2013-03-15 17:40:50 -0400

    nope; didnt do it, reverting

--- client/lib/page.profile.js
+++ client/lib/page.profile.js
@@ -377,8 +377,8 @@ Template.dropboxAlert.events({
 });
 Template.dropboxAlert.text = function(){
     try{
-        //var task = ;
-        return Todos.findOne(Meteor.user().profile.dropbox);
+        var task = Todos.findOne(Meteor.user().profile.dropbox);
+        return task.text;
     }catch(error){
         catch_error('Template.dropboxAlert.text', error, LogLevel.Error, this);
     }
